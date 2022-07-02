626690a06a91dba3a5804b4a75f7a61cc9573520
jamshedhussainjava@gmail.com <Kh@nllha1234>
2022-07-02 16:12:20 +0530

    Adding Structure

---
+++ src/main/java/com/servertech/Z1SbJpaCrud3PostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.servertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z1SbJpaCrud3PostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z1SbJpaCrud3PostgreSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servertech/Z1SbJpaCrud3PostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z1SbJpaCrud3PostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
